5d9e52402a2188470aafd0c9ec81d6a0daa1abcd
EstifanosTaye320 <estifanostaye11@gmail.com>
2024-08-17 10:45:00 +0300

    adding some views and styling

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
+const locals = {
+  title: "Nodejs Blog",
+  description: "Blog application using Nodejs, Express and MongoDB",
+};
+
 router.get("", function (req, res) {
-  res.send("Hello World!");
+  res.render("index", { locals });
+});
+
+router.get("/about", function (req, res) {
+  res.render("about", { locals });
+});
+
+router.get("/contact", function (req, res) {
+  res.render("contact", { locals });
 });
 
 module.exports = router;
